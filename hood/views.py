edf48b79314eb208746cf8b0b4ad133422fef995
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-27 14:02:36 +0300

    create profile in views

--- hood/views.py
+++ hood/views.py
@@ -12,3 +12,18 @@ def index(request):
                                         "current_user":current_user,
                                         "profile":profile,
                                         "posts":posts})
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    test = "Working!!"
+    current_user = request.user
+    if request.method == 'POST':
+        form = CreateProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            new = form.save(commit=False)
+            new.user = current_user
+            new.save()
+            return redirect(view_profile)
+    else:
+        form = CreateProfileForm()
+    return render(request,'profile/create.html',{"test":test,"upload_form":form})
