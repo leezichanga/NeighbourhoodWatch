3984c834fa6a424dd64755fc45971906e5d51a7e
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-27 15:53:26 +0300

    import models in views

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import MyUser,Neighborhood,Post,Business
+
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
