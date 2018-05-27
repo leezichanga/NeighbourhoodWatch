fc13cd725cd8bfbdba456d58335f4fd17b259822
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-27 13:58:33 +0300

    create index view

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    test = "Working!!"
+    current_user = request.user
+    profile = MyUser.get_user()
+    posts = Post.get_post()
+    return render(request,'index.html',{"test":test,
+                                        "current_user":current_user,
+                                        "profile":profile,
+                                        "posts":posts})
