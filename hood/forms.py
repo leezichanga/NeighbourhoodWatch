05f5b9f7db2e61d344f3e4ccdd5c28a395e4fb4e
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-27 16:12:49 +0300

    post form

--- hood/forms.py
+++ hood/forms.py
@@ -4,3 +4,8 @@ class CreateProfileForm(forms.ModelForm):
     class Meta:
         model = MyUser
         exclude = ['user']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['editor','post_date']
