c2b6106c31d856f459d8f32ab24f1d733312afa8
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-27 15:50:41 +0300

    create database and add it to settings

--- neighborhood/neighborhood/settings.py
+++ neighborhood/neighborhood/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'neighborhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'elizabeth',
+        'PASSWORD':'leezie',
     }
 }
 
