cb13b21c21e194ac49a8af12f0f1170ea05be535
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-27 15:29:36 +0300

    neighborhood model

--- hood/models.py
+++ hood/models.py
@@ -2,3 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name =  models.CharField(max_length=60)
+    location = models.CharField(max_length=60)
+    occupants_count = models.PositiveIntegerField(default=0)
+    health_number = models.CharField(max_length=20)
+    police_number = models.CharField(max_length=20)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
