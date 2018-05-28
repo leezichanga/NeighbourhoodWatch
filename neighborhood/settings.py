eb5a0eed18df8edb3849fa2e5968f114a0fe19cf
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-28 18:40:27 +0300

    deployment

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from decouple import config 
-import dj_database_url 
+import dj_database_url
+from decouple import config
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
- SECRET_KEY = config('SECRET_KEY') 
+SECRET_KEY = 'tex0ye@d1a62-ekflc$3aj6v%c0_m)4cqj==p9u_73rp#!_f#r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool) 
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -34,7 +34,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'hood',
+    # 'hood',
+    'hood.apps.HoodConfig',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -87,8 +88,8 @@ DATABASES = {
         'PASSWORD':'leezie',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500) 
-DATABASES['default'].update(db_from_env) 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -127,12 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ 
-os.path.join(BASE_DIR, "static"), 
-] 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
-STATICFILES_STORAGE = 
-'whitenoise.django.GzipManifestStaticFilesStorage' 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
