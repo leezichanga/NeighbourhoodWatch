d2d10bd2af2e67b5b5d2c5c4c83db4241500119f
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-28 18:19:39 +0300

    add asterisks to allowed host in settings

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool) 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
